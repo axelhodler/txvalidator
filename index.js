13171b99c79151c209475ec41851f55f41187d14
Axel Hodler <axel@hodler.co>
2017-08-14 18:07:55 +0200

    validates tx

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const {json} = require('micro')
+const EthereumTx = require('ethereumjs-tx')
 
 module.exports = async (req, res) => {
-  const post = await json(req)
-  res.end(JSON.stringify(post))
+  const transactionPayload = await json(req)
+  var stringifiedTxPayload = JSON.stringify(transactionPayload)
+  var tx = new EthereumTx(JSON.parse(stringifiedTxPayload))
+  if (tx.verifySignature()) {
+    res.end(stringifiedTxPayload)
+  } else {
+    throw new Error("Tx invalid")
+  }
 }
